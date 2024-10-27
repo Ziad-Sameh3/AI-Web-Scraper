0ea943dcc23a39e77b577f8c9cd3735e7e0456a9
Ziad Sameh Said Abdelbaset <98721410+Ziad-Sameh3@users.noreply.github.com>
2024-10-27 16:30:03 +0300

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,21 +1,28 @@
+import streamlit as st
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 
 def scraping(website):
-    print('Launching chrome driver...')
+    st.write("Launching Chrome driver...")
     
-    chrome_path = './chromedriver.exe'
     options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome( options=options)
-    
+    options.add_argument('--headless')  # Ensures no display needed
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
 
+    # Use WebDriver Manager to automatically handle the ChromeDriver setup
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    
+    # Fetch the web page
     driver.get(website)
     html = driver.page_source
+    driver.quit()  # Close the driver after fetching page source
     return html
 
 def extract_body_content(html_content):
-    soup = BeautifulSoup(html_content,'html.parser')
+    soup = BeautifulSoup(html_content, 'html.parser')
     body_content = soup.body
     if body_content:
         return str(body_content)
@@ -24,18 +31,17 @@ def extract_body_content(html_content):
 def clean_body_content(body_content):
     soup = BeautifulSoup(body_content, "html.parser")
     
-    for scriptOrStyle in soup(['script', 'style']):
-        scriptOrStyle.extract()
+    for script_or_style in soup(['script', 'style']):
+        script_or_style.extract()
     
     clean_content = soup.get_text(separator='\n')
     clean_content = "\n".join(
-        line.strip() for line in clean_content.splitlines() if line.split()
+        line.strip() for line in clean_content.splitlines() if line.strip()
     )
     
     return clean_content
 
 def split_cleaned_content(cleaned_content, max_length=6000):
-    
     return [
         cleaned_content[i : i + max_length] for i in range(0, len(cleaned_content), max_length)
-        ]
+    ]
