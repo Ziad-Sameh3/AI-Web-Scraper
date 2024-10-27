22ec3b86ad9787058e96783696eb340ee1ad8d39
Ziad Sameh Said Abdelbaset <98721410+Ziad-Sameh3@users.noreply.github.com>
2024-10-28 02:11:42 +0300

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -10,7 +10,7 @@ def scraping(website):
    
     chrome_path = './chromedriver.exe'
     options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=Service(chrome_path), options=options)
+    driver = webdriver.Chrome(options=options, executable_path='chromedriver.exe', service=Service(chrome_path))
 
     driver.get(website)
     html = driver.page_source
