c42f8127a45a3bcc5e3094d47933963dbf165610
Ziad Sameh Said Abdelbaset <98721410+Ziad-Sameh3@users.noreply.github.com>
2024-10-28 02:36:10 +0300

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,16 +1,14 @@
-import streamlit as st
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
-
 
 def scraping(website):
     print('Launching chrome driver...')
-   
+    
     chrome_path = './chromedriver.exe'
     options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(options=options, service=Service(chrome_path))
+    driver = webdriver.Chrome(service=Service(chrome_path), options=options)
+    
 
     driver.get(website)
     html = driver.page_source
@@ -25,19 +23,19 @@ def extract_body_content(html_content):
 
 def clean_body_content(body_content):
     soup = BeautifulSoup(body_content, "html.parser")
-
+    
     for scriptOrStyle in soup(['script', 'style']):
         scriptOrStyle.extract()
-
+    
     clean_content = soup.get_text(separator='\n')
     clean_content = "\n".join(
         line.strip() for line in clean_content.splitlines() if line.split()
     )
-
+    
     return clean_content
 
 def split_cleaned_content(cleaned_content, max_length=6000):
-
+    
     return [
         cleaned_content[i : i + max_length] for i in range(0, len(cleaned_content), max_length)
         ]
